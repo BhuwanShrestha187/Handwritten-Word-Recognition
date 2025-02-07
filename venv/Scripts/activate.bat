eb79800e9c22e1959517692ca76e98797931bc64
Bhuwan Shrestha <bshrestha2146@conestogac.on.ca>
2025-02-06 22:31:27 -0500

    Ignored venv folder

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Conestoga College\6th Semester\1. Systems Project\Main Project\Handwritten Recognition System\venv
+set VIRTUAL_ENV=D:\Conestoga College\Final System Project\Handwritten-Word-Recognition\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
